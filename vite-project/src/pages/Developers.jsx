aabe02149bbb68548691b5f67501f1399dfeac64
RENEE <wind.autumn@mail.ru>
2025-01-24 17:14:51 +0300

    15

--- vite-project/src/pages/Developers.jsx
+++ vite-project/src/pages/Developers.jsx
@@ -5,9 +5,6 @@ import style from '../header/home.module.css';
 import { Link } from 'react-router-dom';
 
 const Developers = () => {
-    const handleButtonClick = () => {
-        window.location.href = 'https://github.com/ReeneQo/hakawt'; 
-      };
     return (
     <div className={styles.card_general}>
         <header>
@@ -19,12 +16,12 @@ const Developers = () => {
                     <li className={style.li}><Link to='/About'>About us</Link></li>
                     <li className={style.li}><Link to='/Developers'>Developers</Link></li>
                 </ul>
-                <button className={style.button} onClick={handleButtonClick}> <Github/> GitHub </button> 
+                <button className={style.button} onClick={() => window.open('https://github.com/ReeneQo/hakawt', '_blank')}> <Github/> GitHub </button> 
             </nav>
             </div>
         </header>
 
-        <h1 className={styles.h1_dev}>Our Team</h1>
+        {/* <h1 className={styles.h1_dev}>Our Team</h1> */}
 
         <div className={styles.card_container}>
        <div className={styles.card_main}>
@@ -32,7 +29,7 @@ const Developers = () => {
         <h4>Artem (clevergg) Gorbunov</h4>
         <p className={styles.card_text}>Developer</p>
         <hr></hr>
-        <button className={styles.git_button}><Github/></button>
+        <button className={styles.git_button} onClick={() => window.open('https://github.com/clevergg', '_blank')}><Github/></button>
         <p>About you</p>
        </div>
 
@@ -42,7 +39,7 @@ const Developers = () => {
         <h4>Dmitriy (rennee) Zelenin</h4>
         <p className={styles.card_text}>Developer</p>
         <hr></hr>
-        <button className={styles.git_button}><Github/></button>
+        <button className={styles.git_button} onClick={() => window.open('https://github.com/ReeneQo', '_blank')}><Github/></button>
         <p>About you</p>
        </div>
        
@@ -51,7 +48,7 @@ const Developers = () => {
         <h4>Stepan (ierumi) Starostin</h4>
         <p className={styles.card_text}>Developer</p>
         <hr></hr>
-        <button className={styles.git_button}><Github/></button>
+        <button className={styles.git_button} onClick={() => window.open('https://github.com/ierumi', '_blank')}><Github/></button>
         <p>About you</p>
        </div>
        </div>
@@ -63,7 +60,7 @@ const Developers = () => {
         <h4>Valeriy (valeron) Erushev</h4>
         <p className={styles.card_text}>Captian, CEO</p>
         <hr></hr>
-        <button className={styles.git_button}><Github/></button>
+        <button className={styles.git_button} onClick={() => window.open('https://github.com/valeron1380', '_blank')}><Github/></button>
         <p>0% code writing</p>
        </div>
 
@@ -72,10 +69,11 @@ const Developers = () => {
         <h4>Kirill (SAGZZad) Litavrin</h4>
         <p className={styles.card_text}>Developer</p>
         <hr></hr>
-        <button className={styles.git_button}><Github/></button>
+        <button className={styles.git_button} onClick={() => window.open('https://github.com/SAGZZad', '_blank')}><Github/></button>
         <p>About you</p>
        </div>
        </div>
+       <h1 className={styles.h1_dev}>Developers</h1>
     </div>
     )
 };
